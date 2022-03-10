6aeee8b6159b003365a66b8bfc71d9a613cd00dc
000hen <42735043+000hen@users.noreply.github.com>
2022-03-10 20:24:23 +0800

    The First Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# YTDownloader-Browser
+
+This is a Chrome extension that allows you to download videos from YouTube/YouTube Music.
+
+> This is a upgrade version of [YouTube-Downloader](https://github.com/000hen/YouTube-Downloader).
+
+## Features
+
+ * Download multiple YouTube/YouTube Music videos at once.
+ * Download YouTube/YouTube Music videos from a playlist.
+ * Download all your favortie YouTube Music songs.
+ * Intuitive download videos/songs tool.
+
+## Installation
+
+_You can download the executable file from the [release page](https://github.com/000hen/YouTube-Downloader/releases/latest)_
+
+ 1. Download the repository.
+ 2. Install the Chrome extension with developer mode.
+ 3. Enjoy!
+
+## How to Download The Videos or Songs
+
+**Remember the videos/playlists/songs must be public.**
+
+ 1. Open the videos in the main YouTube page or open the songs in the YouTube Music page.
+ 2. Click the extension icon in the top right corner (You may need to pin it).
+ 3. It will download the videos/songs.
+
+Or the download button in the video page or song page.
+ 
+ * In YouTube video page:
+ ![YT-Video-Page-Button](https://cdn.discordapp.com/attachments/698551378745884835/916841755490545744/unknown.png)
+
+ * In YouTube Music playing song page:
+ ![YT-Music-Page-Button](https://cdn.discordapp.com/attachments/698551378745884835/916842535563956244/unknown.png)
+
+ * In YouTube Playlist page:
+ ![YT-Playlist-Page-Button](https://cdn.discordapp.com/attachments/698551378745884835/916842441536053248/unknown.png)
+
+ * In YouTube Music playlist page:
+ ![YT-Music-Playlist-Page-Button](https://cdn.discordapp.com/attachments/698551378745884835/916842636751556608/unknown.png)
+
+## Some of Screenshots
+
+___Note: It will come out, but not now.___
+
+## Enjoy!
---
+++ src/main.js
@@ -0,0 +1,151 @@
+const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
+const uuid = require('uuid');
+const {
+    createFFmpeg,
+    fetchFile
+} = require('@ffmpeg/ffmpeg');
+const ffmpeg = createFFmpeg({
+    log: false
+});
+const fs = require('browserify-fs');
+const downloadProgessLimit = 10;
+
+function downloadAsFile(blob, filename) {
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(new Blob([blob]));
+    link.download = filename;
+    link.click();
+    link.remove();
+
+    return;
+}
+
+function sendPageMessage(message, type) {
+    try {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                message: message,
+                type: type
+            });
+        });
+    } catch (e) { }
+}
+
+async function sendToServer(data) {
+    if (!ffmpeg.isLoaded()) await ffmpeg.load();
+    var nowdwn = 0;
+    var done = 0;
+    var unjson = data;
+    var errVds = [];
+    if (typeof (unjson.videos) === "string") {
+        var o = await ytpl(unjson.videos);
+        unjson.videos = [];
+        o.items.forEach(e => {
+            unjson.videos.push(e.shortUrl);
+        })
+    }
+
+    function download(url) {
+        return new Promise(async (resolve, reject) => {
+            var info = await ytdl.getBasicInfo(url);
+            var title = info.player_response.videoDetails.title;
+            var author = info.player_response.videoDetails.author;
+            var toFilename = string => string.replace(/\n/g, " ").replace(/[<>:"/\\|?*\x00-\x1F]| +$/g, "").replace(/^(CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])$/, x => x + "_");
+            console.log(`\x1b[33mDownloading ${title}, By ${author}...\x1b[0m`);
+            sendPageMessage(`Downloading ${title}, By ${author}...`, "info");
+            if (unjson.type === "video") {
+                // var a = require("./assents/download.js");
+                // resolve(await a.dall(url, title, toFilename(title)))
+            } else {
+                var id = uuid.v4();
+                // // var file = fs.createWriteStream(`songs/${toFilename(title)}.mp3`);
+                var file = []; 
+                
+                var stream = ytdl(url, {
+                    filter: "audioonly"
+                });
+
+                stream.on("data", chunk => {
+                    file.push(chunk);
+                })
+
+                stream.on('end', async () => {
+                    var buffer = Buffer.concat(file);
+                    console.log(`\x1b[33mConverting ${title} from WebM to MP3...\x1b[0m`);
+                    sendPageMessage(`Converting ${title} from WebM to MP3...`, "info");
+                    // downloadAsFile(buffer, "test")
+                    ffmpeg.FS('writeFile', `${id}`, buffer);
+                    await ffmpeg.run('-i', `${id}`, `${id}.mp3`);
+                    // child_process.execSync(`ffmpeg -y -loglevel 0 -hide_banner -i "temp/${id}" -acodec libmp3lame -ab 128k -ar 44100 -ac 2 "${toFilename(title)}.mp3"`);
+                    // fs.unlink(`${id}`);
+                    downloadAsFile(ffmpeg.FS("readFile", `${id}.mp3`), `${toFilename(title)}.mp3`);
+
+                    console.log(`\x1b[32mDownloaded ${title}\x1b[0m`);
+                    sendPageMessage(`Downloaded ${title}`, "success");
+                    resolve(true);
+                });
+                stream.on('error', err => {
+                    file.end();
+                    fs.unlink(`${toFilename(title)}.mp3`);
+                    console.log(`\x1b[31mDownload ${title} Failed: ${err}\x1b[0m`, err);
+                    sendPageMessage(`Download ${title} Failed: ${err}`, "error");
+                    if (errVds.findIndex(e => e === url) === -1) {
+                        unjson.videos.push(url);
+                    }
+                    errVds.push(url);
+                    resolve(false);
+                });
+            }
+
+
+        });
+    }
+
+    var p = 0;
+
+    function dwn() {
+        var dsl = downloadProgessLimit;
+        if (unjson.type === "video") dsl = Math.floor(downloadProgessLimit / 2);
+        if (nowdwn < dsl) {
+            download(unjson.videos[p]).then(e => {
+                nowdwn--;
+                done++;
+                if (done === unjson.videos.length) {
+                    if (errVds.length > 0) {
+                        console.log(`\x1b[31mDownloading Failed: ${errVds.length} videos\x1b[0m`);
+                        sendPageMessage(`Downloading Failed: ${errVds.length} videos`, "error");
+                        for (var video of errVds) {
+                            console.log(`\x1b[31mFailed: ${video}\x1b[0m`);
+                            sendPageMessage(`Failed: ${video}`, "error");
+                        }
+                    }
+                    console.log(`\x1b[32mDownloaded all file, in this session YouTube-Downloader downloaded ${p} file(s)\x1b[0m`);
+                    sendPageMessage(`Downloaded all file, in this session YouTube-Downloader downloaded ${p} file(s)`, "success");
+                }
+            });
+            nowdwn++;
+            p++;
+        }
+
+        if (p !== unjson.videos.length && done + 1 !== unjson.videos.length) {
+            setTimeout(dwn, 500)
+        }
+    }
+    dwn();
+
+    return true;
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'download') {
+        chrome.tabs.sendMessage(sender.tab.id, {
+            message: 'download'
+        });
+        return;
+    }
+    sendToServer(request);
+});
